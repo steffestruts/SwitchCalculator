0aaf175b457edca4d167d482be52001bc0ef10f8
Stefan Sjöberg <bird.nerd@outlook.com>
2025-02-17 14:53:53 +0100

    Add project files.

---
+++ MainApp/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Skriv första talet:");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Vilken operator vill du använda: (+, -, *, /):");
+string typeSymbole = Console.ReadLine()!;
+Console.WriteLine("Skriv andra talet:");
+int numberB = int.Parse(Console.ReadLine()!);
+
+switch (typeSymbole)
+{
+    case "+":
+        Console.Clear();
+        Console.WriteLine(numberA + numberB);
+        break;
+    case "-":
+        Console.Clear();
+        Console.WriteLine(numberA - numberB);
+        break;
+    case "*":
+        Console.Clear();
+        Console.WriteLine(numberA * numberB);
+        break;
+    case "/":
+        Console.Clear();
+        Console.WriteLine(numberA / numberB);
+        break;
+    default:
+        Console.WriteLine("Felaktig operator");
+        break;
+}
+
+Console.ReadKey();
